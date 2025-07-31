570648e1e9bc36bf63cde03a6a6c84e4b5b3c485
djxpro1234 <dhananjaysode428@gmail.com>
2025-07-31 15:57:29 +0530

    1.5

---
+++ js/pop.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  document.getElementById("popup").style.display = "flex";
+});
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+
