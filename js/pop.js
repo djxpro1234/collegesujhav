f6c017b00e07a4b5b74df42d890b7df65c8b073d
djxpro1234 <dhananjaysode428@gmail.com>
2025-08-01 15:17:43 +0530

    1.6

--- js/pop.js
+++ js/pop.js
@@ -2,8 +2,23 @@ window.addEventListener("load", () => {
   document.getElementById("popup").style.display = "flex";
 });
 
+
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
 
+const menuToggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+});
+
+// Toggle dropdowns for mobile
+dropdownToggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    const parent = toggle.parentElement;
+    parent.classList.toggle('open');
+  });
+});
